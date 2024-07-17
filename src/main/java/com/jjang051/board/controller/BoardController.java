399acbf4ef4dead60ddb0758b9312361b5dc23c8
jjang051jhta <jjang051jhta@gmail.com>
2024-07-17 12:49:24 +0900

    init

--- src/main/java/com/jjang051/board/controller/BoardController.java
+++ src/main/java/com/jjang051/board/controller/BoardController.java
@@ -1,15 +1,36 @@
 package com.jjang051.board.controller;
 
+import com.jjang051.board.dto.CustomUserDetails;
+import com.jjang051.board.entity.Board;
+import com.jjang051.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+    private final BoardRepository boardRepository;
     @GetMapping("/write")
     public String insert() {
         return "board/write";
     }
+
+
+    @PostMapping("/write")
+    public String insert(@RequestParam String title, @RequestParam String content,
+                         @AuthenticationPrincipal CustomUserDetails customUserDetails)    {
+        Board saveBoard = Board.builder()
+                .title(title)
+                .content(content)
+                .writer(customUserDetails.getLoggedMember())
+                .build();
+        boardRepository.save(saveBoard);
+        return "redirect:/board/write";
+    }
+
+
 
 }
--- src/main/java/com/jjang051/board/entity/Board.java
+++ src/main/java/com/jjang051/board/entity/Board.java
@@ -1,10 +1,16 @@
 package com.jjang051.board.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
@@ -19,4 +25,12 @@ public class Board {
     private Member writer;
 
     private LocalDateTime regDate;
+
+    @Builder
+    public Board(String title, String content, Member writer, LocalDateTime regDate) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.regDate = regDate;
+    }
 }
