652d2cc3b804b9e0836385f3d1239c849b2f110b
jjang051jhta <jjang051jhta@gmail.com>
2024-07-19 15:14:24 +0900

    init

--- src/main/java/com/jjang051/board/repository/BoardRepository.java
+++ src/main/java/com/jjang051/board/repository/BoardRepository.java
@@ -5,6 +5,7 @@ import com.jjang051.board.entity.Board;
 import com.jjang051.board.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,6 +14,14 @@ public interface BoardRepository extends JpaRepository<Board,Long> {
     @Query("select b from Board b left join fetch b.comments")
     List<Comment> findByBoardFetch();
 
+    @Query(value=
+            "select b from Board b where b.title like % :keyword % "
+            )
+    List<Comment> findBySubject(@Param("keyword") String keyword);
 
-
+    @Query(value=
+            "select * from Board b where b.title like % :keyword % ",
+            nativeQuery = true
+    )
+    List<Comment> findBySubject02(@Param("keyword") String keyword);
 }
